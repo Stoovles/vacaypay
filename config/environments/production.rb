ae03c2858a5020f04332ae088069a5cc03f047e3
Ethan Grab <ethangrab@gmail.com>
2020-06-23 14:48:20 -0600

    Changed all old heroku domain names to updated domain name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://cryptic-brook-29556.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://cryptic-brook-29556.herokuapp.com/' ]
+  config.action_cable.url = 'wss://powerful-everglades-07534.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://powerful-everglades-07534.herokuapp.com/' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
